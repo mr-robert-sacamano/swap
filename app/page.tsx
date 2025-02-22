442ebd1d128cf839a553adb276436eb6dac82d19
Gabe Fahl <gf18@duke.edu>
2025-02-22 11:21:38 -0500

    homepage updates

--- app/page.tsx
+++ app/page.tsx
@@ -10,21 +10,24 @@ export default function Home() {
           <p>BETH began as a meme but grew into a thriving community on Solana. <span className="logo">BethSwap</span> continues that momentum in the decentralized finance (DeFi) space, making it simpler and more rewarding for BETH holders to trade, stake, and interact. <span className="logo">BethSwap</span> is more than just a DEX; it’s where the memecoin culture meets genuine utility and community-driven rewards.</p>          
         </div>                      
       </div>
-      <div className="flex flex-col gap-6">
-        <div className="">
-          
+      <div className="max-w-4xl mx-auto p-12 flex flex-col gap-6 text-[#ca41c5] text-lg">
+        <div className="p-12 border-[#ca41c5] border-2 shadow-md rounded-lg">
+          <span className="font-bold text-5xl dynapuff">Swap</span><br />Swap boasts efficient and optimized token swaps, ensuring users get the best value through intelligent routing and liquidity aggregation on the Solana blockchain.
         </div>
 
-        <div className="">
-          
+        <div className="p-12 border-[#ca41c5] border-2 shadow-md rounded-lg">
+        <span className="font-bold text-5xl dynapuff">Rewards</span><br />The rewards system enable BETH holders to stake their tokens and earn passive income, aligning personal incentives with the ecosystem’s long-term growth.
+
         </div>
 
-        <div className="">
-          
+        <div className="p-12 border-[#ca41c5] border-2 shadow-md rounded-lg">
+        <span className="font-bold text-5xl dynapuff">Sweep</span><br /> Sweep your dust and unwanted tokens into BETH with BethSweep!
+
         </div>
 
-        <div className="">
-          
+        <div className="p-12 border-[#ca41c5] border-2 shadow-md rounded-lg">
+        <span className="font-bold text-5xl dynapuff">Club</span><br /> Club BETH is an exclusive space for active BETH holders, offering direct access to the community and team insights.
+
         </div>                        
       </div>
     </>
